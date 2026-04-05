8521c47535588c993d2b6e257027a98347172178
Muhammedkhaled1996 <muhammedkhaled25@gmail.com>
2026-04-05 02:26:29 +0200

    edit proxy name

--- Ecommerce/src/middleware.ts
+++ Ecommerce/src/middleware.ts
@@ -1,8 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function proxy(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
   }
@@ -10,5 +10,5 @@ export async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/cart", "/cart/:path*", "/orders", "/orders/:path*"],
+  matcher: ["/cart/:path*", "/orders/:path*"],
 };
